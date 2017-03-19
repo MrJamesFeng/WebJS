c63440413e8a237782c212503f754944b8d22dfd
MrJamesFeng <812463918@qq.co>
2017-03-19 14:21:52 +0800

    更换GMPlate

--- webViewJsHtml/webViewJsHtml/ViewController.m
+++ webViewJsHtml/webViewJsHtml/ViewController.m
@@ -6,7 +6,6 @@
 //  Copyright © 2017年 LDY. All rights reserved.
 //
 #import "ViewController.h"
-#import "MGTemplateEngine.h"
 
 @interface ViewController ()<UIWebViewDelegate>
 
@@ -20,10 +19,27 @@
     [super viewDidLoad];
     
     [self.view addSubview:self.mywebView];
-    NSURL *url = [NSURL URLWithString:@"www.baidu.com"];
-    [self.mywebView loadHTMLString:[self htmlString:@"<p>内容</p>" title:@"baidu"] baseURL:url];
+//    NSURL *url = [NSURL URLWithString:@"www.baidu.com"];
+//    [self.mywebView loadHTMLString:[self htmlString:@"<p>内容</p>" title:@"baidu"] baseURL:url];
     
 }
+/*
+-(void)setupMGTemplateEngine{
+    MGTemplateEngine *templateEngine = [MGTemplateEngine templateEngine];
+    ICUTemplateMatcher *templateMatcher = [ICUTemplateMatcher matcherWithTemplateEngine:templateEngine];
+    [templateEngine setMatcher:templateMatcher];
+    
+    [templateEngine setObject:@"content" forKey:@"title"];
+    [templateEngine setObject:@"title" forKey:@"title"];
+    
+    NSString *htmlPath = [[NSBundle mainBundle]pathForResource:@"test" ofType:@"html"];
+    NSString *htmlString = [templateEngine processTemplateInFileAtPath:htmlPath withVariables:nil];
+    
+    NSString *baseUrl = [[[NSBundle mainBundle]resourcePath]stringByAppendingPathComponent:@"test"];
+    [_mywebView loadHTMLString:htmlString baseURL:[NSURL URLWithString:baseUrl]];
+    
+}
+ */
 #pragma mark- interactive methold
 
 /**
